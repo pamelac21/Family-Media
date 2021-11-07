c4c2409197469dd235b347a7794746c4672a5eed
spurgason <shanepurgason.98@gmail.com>
2021-11-07 18:11:33 -0500

    seeds run & server starts

--- seeds/index.js
+++ seeds/index.js
@@ -9,9 +9,13 @@ const seedtodos = require('./todo-seeds');
 const seedAll = async() => {
     await sequelize.sync({ force: true });
     await seedUsers();
+    console.log('--------------');
     await seedPosts();
+    console.log('--------------');
     await seedComments();
+    console.log('--------------');
     await seedtodos();
+    console.log('--------------');
     process.exit(0);
 };
 
