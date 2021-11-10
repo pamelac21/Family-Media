6982f5a335698807cb5516e50f94e3e040a46f5c
spurgason <shanepurgason.98@gmail.com>
2021-11-10 13:06:08 -0500

    added photo routes

--- controllers/index.js
+++ controllers/index.js
@@ -4,10 +4,12 @@ const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 const calendarRoutes = require('./calendar-routes');
+const photosRoutes = require('./photos-routes');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 router.use('/calendar', calendarRoutes);
+router.use('/photos', photosRoutes);
 
 module.exports = router;
